22cf5c79a53d7038e1747b8704a1725b67d07c1f
Jean Maximus Calleja Cacacho <maxcacacho@gmail.com>
2026-01-19 01:51:57 +0800

    feat: put HF model hello world

--- backend/rag-scripts.py
+++ backend/rag-scripts.py
@@ -9,3 +9,26 @@ naive retrieval augmented generation (RAG).
 In further iterations of this project, I intend to
 turn this into a proper backend with FastAPI.
 """
+
+from dotenv import load_dotenv
+import os
+from huggingface_hub import InferenceClient
+
+load_dotenv()
+
+client = InferenceClient(
+    provider="hf-inference",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+result = client.sentence_similarity(
+    "Are you excited to study?",
+    [
+        "I want to prepare better for tests.",
+        "I want to develop good study habits.",
+        "I want to re-invent my perception of classroom lectures.",
+    ],
+    model="sentence-transformers/all-MiniLM-L6-v2",
+)
+
+print(result)
